d4217483182fbe3274c16b14a39e654693f5c32e
darkfrei <34188677+darkfrei@users.noreply.github.com>
2020-12-27 23:31:50 +0100

    Updated function update_nn (nn, input)

--- universal-neural-network-04/unn.lua
+++ universal-neural-network-04/unn.lua
@@ -21,14 +21,12 @@
 
 serpent = require('serpent')
 
-function new_weight ()
-	return 4*math.random()-2 -- from -2 to 2
-end
 
-activation = {}
 
-activation[#activation+1] = function (value, derivative)
-	-- ReLU [++-: monotonic, monotonic derivative, appr. identity near origin]
+do_activation = {}
+
+do_activation[#do_activation+1] = function (value, derivative)
+	-- 1. ReLU [++-: monotonic, monotonic derivative, appr. identity near origin]
 	if not derivative then
 		return value>0 and value or 0
 	else
@@ -36,8 +34,8 @@ activation[#activation+1] = function (value, derivative)
 	end
 end
 
-activation[#activation+1] = function (value, derivative)
-	-- Leaky ReLU [++-]
+do_activation[#do_activation+1] = function (value, derivative)
+	-- 2. Leaky ReLU [++-]
 	if not derivative then
 		return value>0 and value or 0.01*value
 	else
@@ -45,8 +43,8 @@ activation[#activation+1] = function (value, derivative)
 	end
 end
 
-activation[#activation+1] = function (value, derivative)
-	-- TanH [+-+]
+do_activation[#do_activation+1] = function (value, derivative)
+	-- 3. TanH [+-+]
 	local f = math.tanh(value)
 	if not derivative then
 		return f
@@ -55,8 +53,8 @@ activation[#activation+1] = function (value, derivative)
 	end
 end
 
-activation[#activation+1] = function (value, derivative)
-	-- ISRLU [+++]
+do_activation[#do_activation+1] = function (value, derivative)
+	-- 4. ISRLU [+++]
 	if not derivative then
 		local f = value/math.sqrt(1+value^2)
 		return (value>0) and value or f
@@ -66,8 +64,8 @@ activation[#activation+1] = function (value, derivative)
 	end
 end
 
-activation[#activation+1] = function (value, derivative)
-	-- ELU
+do_activation[#do_activation+1] = function (value, derivative)
+	-- 5. ELU
 	local a = 1
 	local f = a*(math.exp(value)-1)
 	if not derivative then
@@ -77,8 +75,8 @@ activation[#activation+1] = function (value, derivative)
 	end
 end
 
-activation[#activation+1] = function (value, derivative)
-	-- sin
+do_activation[#do_activation+1] = function (value, derivative)
+	-- 6. sin
 	if not derivative then
 		return math.sin(value)
 	else
@@ -86,8 +84,8 @@ activation[#activation+1] = function (value, derivative)
 	end
 end
 
-activation[#activation+1] = function (value, derivative)
-	-- gauss
+do_activation[#do_activation+1] = function (value, derivative)
+	-- 7. gauss
 	local f = math.exp(-value^2)
 	if not derivative then
 		return f
@@ -96,9 +94,16 @@ activation[#activation+1] = function (value, derivative)
 	end
 end
 
+-------------- functions
 
+function new_weight ()
+	return 4*math.random()-2 -- from -2 to 2
+end
 
-	
+function random_n_activation ()
+	return math.random(#do_activation) -- number of activation
+end
+
 
 function create_nn (input, middle, output)
 	local nn = {n_inputs = input, n_middles = middle, n_outputs = output}
@@ -110,11 +115,14 @@ function create_nn (input, middle, output)
 		for to = math.max((input+1), (from+1)), (input+middle+output) do -- to: from first middle to last output
 			local weight = new_weight ()
 			table.insert(weights, weight)
-			local connection={from=from, to=to, nweight=#weights, activation=1}
+			local n_activation = random_n_activation ()
+			table.insert (activations, n_activation)
+			local connection={from=from, to=to, nweight=#weights, activation=#activations}
 			table.insert(connections, connection)
 		end
 	end
 	nn.weights=weights
+	nn.activations=activations
 	nn.connections=connections
 	return nn
 end
@@ -122,7 +130,30 @@ end
 
 function update_nn (nn, input)
 	local output = {}
-	
+	local nodes = {}
+	for i, input_value in pairs (input) do -- copy values from input
+		nodes[i] = input[i]
+	end
+	for i, connection in ipairs (nn.connections) do
+		local weight = nn.weights[connection.nweight]
+		local from = connection.from
+--		print ('activations '..serpent.serialize(nn.activations, {indent = '	', sortkeys = false, comment = false}))
+--		print ('connection.activation' .. connection.activation)
+		local n_activation = nn.activations[connection.activation]
+--		print ('n_activation'..n_activation)
+		local func = do_activation[n_activation]
+		local value = func(nodes[from])
+		value = weight*value
+		local to = connection.to
+		if to < (nn.n_inputs+nn.n_middles+1) then -- write to middle node
+			
+			nodes[to] = nodes[to] and nodes[to] + value or value
+		else -- write to output node
+			print('i'..i..' to'..to)
+			to = to - (nn.n_inputs+nn.n_middles)
+			output[to] = output[to] and (output[to] + value) or value
+		end
+	end
 	return output
 end
 
@@ -153,10 +184,17 @@ end
 
 
 ------ test
-local nn = create_nn (2, 2, 1) -- 
-print (#nn.weights)
+local example = {{0, 1},{1, 0}} -- example with two inputs and one output values
+local nn = create_nn (#example[1], 2, #example[2]) -- 
+print (serpent.serialize(nn, {indent = '	', sortkeys = false, comment = false}))
+
+local output = update_nn (nn, example[1])
+
+--print ('weights '.. #nn.weights)
+
+
 
 --print (serpent.block(nn), {comment =false, sortkeys=false})
 --print (serpent.line(nn), {comment =false, sortkeys=false})
 --print (serpent.dump(nn), {comment =false, sortkeys=false})
-print (serpent.serialize(nn, {indent = '	', sortkeys = false, comment = false}))
+print ('output '..serpent.serialize(output, {indent = '	', sortkeys = false, comment = false}))
